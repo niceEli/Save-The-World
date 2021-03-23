11f80167476d626a0ab26123671481325cd591e9
niceEli <78963198+niceEli@users.noreply.github.com>
2021-03-23 12:58:45 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -443,6 +443,182 @@ f f 4 2 2 e d 1 b f f 2 e f f .
 . . 4 4 e . f e f f f f f . . . 
 . . . . . . . f f . . . . . . . 
 `];
+            case "anim7":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "anim8":return [img`
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+`];
+            case "anim9":return [img`
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+`];
+            case "anim10":return [img`
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 2 2 2 e d d 4 . . . . . 
+. . . f 2 2 2 e d d e . . . . . 
+. . . f 5 5 4 f e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`];
+            case "anim11":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . 4 4 e . f e f f f f f . . . 
+. . 4 d 4 f e e e f e f f f . . 
+. . f f f e e 4 f f 2 e f f f . 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+. f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+f f 4 2 2 4 d f f e f 2 e 2 f f 
+f f 4 2 2 e d 1 b f f 2 e f f . 
+. . f f f e e 4 f f 2 e f f f . 
+. . 4 d 4 f e e e f e e f f . . 
+. . 4 4 e . f e f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+`];
+            case "anim12":return [img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 2 2 f f f f f . . 
+. . f f e 2 e 2 2 e 2 e f f . . 
+. . f e 2 f 2 f f 2 f 2 e f . . 
+. . f f f 2 2 e e 2 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 f f f f f f f f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`];
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     if (controller.A.isPressed()) {
-        if (info.score() <= 5) {
+        if (info.score() >= 5) {
             tiles.setTilemap(tilemap`level1`)
             game.showLongText("You Saved the world", DialogLayout.Full)
             info.stopCountdown()
@@ -9,6 +9,29 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, l
     } else {
         mySprite.say("I should press A (Z)", 500)
     }
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `],
+    0,
+    false
+    )
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     controller.moveSprite(mySprite, 200, 200)
@@ -220,6 +243,31 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         `],
     100,
     true
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . 4 4 e . f e f f f f f . . . 
+        . . 4 d 4 f e e e f e f f f . . 
+        . . f f f e e 4 f f 2 e f f f . 
+        f f 4 2 2 e d 1 b f f 2 e f f . 
+        f f 4 2 2 4 d f f e f 2 e 2 f f 
+        . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+        . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+        f f 4 2 2 4 d f f e f 2 e 2 f f 
+        f f 4 2 2 e d 1 b f f 2 e f f . 
+        . . f f f e e 4 f f 2 e f f f . 
+        . . 4 d 4 f e e e f e e f f . . 
+        . . 4 4 e . f e f f f f f . . . 
+        . . . . . . . f f . . . . . . . 
+        `],
+    100,
+    false
     )
 })
 info.onCountdownEnd(function () {
@@ -490,6 +538,7 @@ game.setDialogFrame(img`
     11bbbbbbbbbbbbbbbbbbbb11
     `)
 tiles.setTilemap(tilemap`level1`)
+scene.setBackgroundColor(9)
 for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
     moola = sprites.create(img`
         . . b b b b . . 
@@ -504,7 +553,6 @@ for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
     tiles.setTileAt(value, sprites.dungeon.floorDark0)
     tiles.placeOnTile(moola, value)
 }
-game.splash("Save The World", "By Eli")
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -524,9 +572,95 @@ mySprite = sprites.create(img`
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 200, 0)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(18, 18))
 scene.cameraFollowSprite(mySprite)
+info.setLife(5)
+ded = 0
+_2d = 1
+mySprite.ay = 500
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile3`)
+game.splash("Save The World", "By Eli")
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . . . . f f . . . . . . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . 4 d 4 f e e e f e f f f . . 
+    . . f f f e e 4 f f 2 e f f f . 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    . . f f f e e 4 f f 2 e f f f . 
+    . . 4 d 4 f e e e f e e f f . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . 4 d 4 f e e e f e f f f . . 
+    . . f f f e e 4 f f 2 e f f f . 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    . . f f f e e 4 f f 2 e f f f . 
+    . . 4 d 4 f e e e f e e f f . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . 4 d 4 f e e e f e f f f . . 
+    . . f f f e e 4 f f 2 e f f f . 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    . f 5 2 2 4 d d 4 e f 2 e 2 2 f 
+    f f 4 2 2 4 d f f e f 2 e 2 f f 
+    f f 4 2 2 e d 1 b f f 2 e f f . 
+    . . f f f e e 4 f f 2 e f f f . 
+    . . 4 d 4 f e e e f e e f f . . 
+    . . 4 4 e . f e f f f f f . . . 
+    . . . . . . . f f . . . . . . . 
+    `],
+500,
+true
+)
+pause(1000)
 scene.cameraShake(5, 1000)
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . f f f f f f . . . . . . 
+    . . . f 2 f e e e e f f . . . . 
+    . . f 2 2 2 f e e e e f f . . . 
+    . . f e e e e f f e e e f . . . 
+    . f e 2 2 2 2 e e f f f f . . . 
+    . f 2 e f f f f 2 2 2 e f . . . 
+    . f f f e e e f f f f f f f . . 
+    . f e e 4 4 f b e 4 4 e f f . . 
+    . . f e d d f 1 4 d 4 e e f . . 
+    . . . f d d d d 4 e e e f . . . 
+    . . . f e 4 4 4 e e f f . . . . 
+    . . . f 2 2 2 e d d 4 . . . . . 
+    . . . f 2 2 2 e d d e . . . . . 
+    . . . f 5 5 4 f e e f . . . . . 
+    . . . . f f f f f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `],
+100,
+false
+)
 music.playTone(262, music.beat(BeatFraction.Half))
 game.showLongText("Eli: Wow i think its a earthquake", DialogLayout.Bottom)
 music.playTone(262, music.beat(BeatFraction.Half))
@@ -536,10 +670,6 @@ game.showLongText("Mocca: Ya", DialogLayout.Left)
 music.playTone(262, music.beat(BeatFraction.Half))
 game.showLongText("Roz And Mocca: Uh Oh", DialogLayout.Center)
 music.playTone(262, music.beat(BeatFraction.Half))
-info.setLife(5)
-ded = 0
-_2d = 1
-mySprite.ay = 500
 forever(function () {
     if (controller.up.isPressed()) {
     	
